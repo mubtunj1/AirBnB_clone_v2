e8dce99ab55f4e385709eac9ec4402d63e93a958
MissJuly <joygitau98@gmail.com>
2023-04-18 12:57:51 +0300

    Fix errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import models
 from datetime import datetime
-from models import storage
+from sqlalchemy import Column, Integer, String, DateTime
 
 
+Base = declarative_base()
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-
 class BaseModel:
-    """A base class for all hbnb models"""
+    """Base class defines all common attributes/methods
+    for other classes
+    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """Initialization of the base model
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
@@ -26,21 +42,38 @@ class BaseModel:
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """Returns a string
+        Return:
+            returns a string of class name, id and dictionary
+        """
+        # cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(
+            type(self).__name__, self.id, self.__dict__)
+            #format(cls, self.id, self.__dict__)
+
+    def __repr__(self):
+        """return a string representation"""
+        return self.__str__()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        """creates dictionary of the class and returns
+        Return:
+            returns a dictionary of all key values in __dict__
+        """
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """delete object"""
+        models.storage.delete(self)
