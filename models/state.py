6dd450863290b267b5f966551af861a03f824b2a
MissJuly <joygitau98@gmail.com>
2023-04-18 14:25:39 +0300

    Fix errors

--- models/state.py
+++ models/state.py
@@ -16,7 +16,9 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
     @property
     def cities(self):
         var = models.storage.all()
