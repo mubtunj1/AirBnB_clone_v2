14989e27014b0362629452511e877e4d5c00bbf2
MissJuly <joygitau98@gmail.com>
2023-05-05 20:16:41 +0300

    Fix task 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -78,7 +78,7 @@ file { '/var/www':
 
 -> file { '/var/www/html/404.html':
     ensure  => 'present'
-    content => $nginx_conf
+    content => "Ceci n'est pas une page\n"
 }
 
 -> file { '/etc/nginx/sites-available/default':
