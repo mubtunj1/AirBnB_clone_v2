6b50d52cfb80899bc6a013fd59e3bbe00f7304c6
MissJuly <joygitau98@gmail.com>
2023-05-05 18:47:01 +0300

    Fix task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -47,7 +47,7 @@ def do_deploy(archive_path):
     if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web-static/releases/{}/".
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
