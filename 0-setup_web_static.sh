bf29960c463689c835e15e08141f9b9347ed970a
Mubtunj1 <mubtunji01@gmail.com>
2023-05-06 08:53:24 +0100

    setup webstatic

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo Ln -s-f /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sodo sed -i '/listen 80 default_server; /a location /hbnb_static/ { alias /data/web_static/curent/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
