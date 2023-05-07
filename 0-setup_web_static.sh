a23eff4bb1c543ee23f69b53187ae63f0228f7bd
Mubtunj <mubtunji01@gmail.com>
2023-05-07 18:48:11 +0100

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static
+# Script that sets up your web servers
+# for the deployment of web_static
+
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
@@ -10,3 +12,4 @@ sudo ln -s-f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server; /a location /hbnb_static/ { alias /data/web_static/current;}' /etc/nginx/sites-available/default
 sudo service nginx restart
+
