b7d7772b3fecddfc0db1466fd6304dbfafca46fd
Mubtunj1 <mubtunji01@gmail.com>
2023-05-07 22:33:01 +0100

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,59 @@
 #!/usr/bin/python3
-"""Distributes an archive to web servers"""
-import os.path
-from fabric.api import env, put, run
+"""
+Creates and distributes an archive to your web servers,
+using the function
+"""
+
+from fabric.api import *
+from datetime import datetime
+from os import path
 
 
 env.hosts = ["54.146.66.65", "34.207.212.18"]
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server"""
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    """Deploy web files to server"""
+    try:
+        if not (path.exists(archive_path)):
+            return False
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        # upload archive to server
+        put(archive_path, '/tmp/')
+
+        # create directory to uncompress files
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/\
+            releases/web_static_{}/'.format(timestamp))
+
+        # uncompress archive to new dir
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+            /data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+
+        # delete archive from web server
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+        # move files out of web_static dir
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
+
+        # remove empty dir
+        run('sudo rm -rf /data/web_static/releases/\
+            web_static_{}/web_static'
+            .format(timestamp))
+
+        # delete old sym link
+        run('sudo rm -rf /data/web_static/current')
+
+        # create new sym link
+        run('sudo ln -s /data/web_static/releases/\
+        web_static_{}/ /data/web_static/current'.format(timestamp))
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+
+        # return True if all operations successful
     return True
