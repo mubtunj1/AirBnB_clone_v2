94c95eb30a5da35c8798178fad74875e4554997f
Mubtunj <mubtunji01@gmail.com>
2023-05-08 09:56:01 +0100

    Improved Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,9 @@ sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s-f /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo chgrp -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server; /a location /hbnb_static/ { alias /data/web_static/current;}' /etc/nginx/sites-available/default
