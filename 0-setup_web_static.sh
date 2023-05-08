372d5149715d099fdbc9df6d3353f4eba26c71ff
Mubtunj <mubtunji01@gmail.com>
2023-05-08 10:28:35 +0100

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,5 +12,6 @@ echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo chgrp -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server; /a location /hbnb_static/ { alias /data/web_static/current;}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
 sudo service nginx restart
