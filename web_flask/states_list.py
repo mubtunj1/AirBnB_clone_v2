288b506aca8b1e8f6f63e45e518692462e43590b
Mubtunj1 <mubtunji01@gmail.com>
2023-05-25 11:35:17 +0100

    fix flask

---
+++ web_flask/states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask("__name__")
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page"""
+    state = storage.all()
+    return render_template('states_list.html', states=states)
+
+
+@app.teardown_appcontext()
+def teardown(self):
+    storage.close()
